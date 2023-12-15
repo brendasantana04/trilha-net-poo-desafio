0933d23b3178b6888a0130d3b960fc85105f8905
brendasantana04 <brenda.santos33@fatec.sp.gov.br>
2023-12-15 14:54:49 -0300

    Implementando codigo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+//realizando testes com nokia
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "333", modelo: "final" , imei: "87886752" , memoria: 124);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Pou");
+
+//realizando testes com iphone
+Console.WriteLine("Smartphone IPhone: ");
+Smartphone iphone = new Iphone(numero: "444", modelo: "beta" , imei: "990898877" , memoria: 32);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Pou");
